965db7af8858c0302fb81b69b0283f1eea48fa2e
Adriana Bermudez <adriana.berm@gmail.com>
2020-10-23 18:59:17 -0500

    initial commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+        $('.hero-slider').slick({
+          autoplay: true,
+          autoplaySpeed: 500,
+          arrows: false,
+          infinite: true,
+          pauseOnFocus: false
+        });
+
+        $('.slider-two').slick({
+                arrows: false,
+                dots: true
+        });
+
+        $('.slider-three').slick({
+                arrows: false,
+                dots: true
+        });
+
+        $('.slider-four').slick({
+                arrows: false,
+                dots: true
+        });
+
+
+});
+
